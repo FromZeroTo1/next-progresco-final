be1f9ef99ba9fd760ec5e5eb7cc26109874578c0
FromZeroTo1 <mark.makarov98@mail.ru>
2023-10-23 22:01:14 +0400

    first commit

--- next.config.js
+++ next.config.js
@@ -12,11 +12,13 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: 'https://nestjs-app-deploy-production-0c94.up.railway.app/api/:path*',
+				destination:
+					'https://nestjs-app-deploy-production-0c94.up.railway.app/api/:path*',
 			},
 			{
 				source: '/uploads/:path*',
-				destination: 'https://nestjs-app-deploy-production-0c94.up.railway.app/uploads/:path*',
+				destination:
+					'https://nestjs-app-deploy-production-0c94.up.railway.app/uploads/:path*',
 			},
 		]
 	},
