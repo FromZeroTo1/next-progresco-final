de7ac932b4f521c12fb53bcdeb8cef240fba8df2
FromZeroTo1 <mark.makarov98@mail.ru>
2023-10-30 01:41:34 +0400

    first commit

--- next.config.js
+++ next.config.js
@@ -12,11 +12,11 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: 'https://ill-rose-monkey-veil.cyclic.app/api/:path*',
+				destination: 'https://nice-gold-scarab-slip.cyclic.app/api/:path*',
 			},
 			{
 				source: '/uploads/:path*',
-				destination: 'https://ill-rose-monkey-veil.cyclic.app/uploads/:path*',
+				destination: 'https://nice-gold-scarab-slip.cyclic.app/uploads/:path*',
 			},
 		]
 	},
